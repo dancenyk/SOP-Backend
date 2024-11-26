9e838aafedfa4ce16c7b355d312a7d1198360456
Danceny Karla <dancenykarla@gmail.com>
2024-11-26 16:01:15 +0100

    database connected

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
+const {dbConnection} = require("./config/db")
+
+dbConnection(); 
 
 app.listen(PORT, ()=>{
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
