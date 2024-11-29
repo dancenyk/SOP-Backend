abd5a4a39c866601fde84c4c098bc662cbb519bf
Danceny Karla <dancenykarla@gmail.com>
2024-11-29 10:13:01 +0100

    cors, service account and firebase admiin

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 require('dotenv').config();
+const admin= require("firebase-admin")
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const profileRoutes = require("./routes/profile")
+const postsRoutes = require("./routes/posts")
+const {dbConnection} = require("./config/db")
 
 const PORT = process.env.PORT || 8080;
-const {dbConnection} = require("./config/db")
+
+
+app.use("/", profileRoutes)
+app.use("/",postsRoutes )
 
 dbConnection(); 
 
