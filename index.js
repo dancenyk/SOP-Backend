20030ea59139b87307e6e431f6baced11909f1ed
Danceny Karla <dancenykarla@gmail.com>
2024-12-02 15:17:39 +0100

    fix: order and route auth added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cors = require("cors")
 require('dotenv').config();
 const admin= require("firebase-admin")
 
+const authRoutes = require("./routes/auth")
+const profileRoutes = require("./routes/profile")
+const postsRoutes = require("./routes/posts")
+const {dbConnection} = require("./config/db")
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -13,18 +18,16 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const profileRoutes = require("./routes/profile")
-const postsRoutes = require("./routes/posts")
-const {dbConnection} = require("./config/db")
-
 const PORT = process.env.PORT || 8080;
 
-
-app.use("/", profileRoutes)
-app.use("/",postsRoutes )
+app.use("/api/auth", authRoutes)
+app.use("/api/profile", profileRoutes)
+app.use("/api/posts",postsRoutes )
 
 dbConnection(); 
 
 app.listen(PORT, ()=>{
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
 })
+
+
